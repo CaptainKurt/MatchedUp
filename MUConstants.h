58dfa2eb6bc494e8c00a1ed8ae4cecf0d2452d54
CaptainKurt <walkkg12@wfu.edu>
2014-08-14 00:25:38 -0400

    Added constants and requests

---
+++ MUConstants.h
@@ -0,0 +1,21 @@
+//
+//  MUConstants.h
+//  MatchedUp
+//
+//  Created by Kurt Walker on 8/14/14.
+//  Copyright (c) 2014 Matched Up. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MUConstants : NSObject
+
+extern NSString *const kCCUserProfileKey;
+extern NSString *const kCCUserProfileNameKey;
+extern NSString *const kCCUserProfileFirstNameKey;
+extern NSString *const kCCUserProfileLocationKey;
+extern NSString *const kCCUserProfileGenderKey;
+extern NSString *const kCCUserProfileBirthdayKey;
+extern NSString *const kCCUserProfileInterestedInKey;
+
+@end
---
+++ MUConstants.m
@@ -0,0 +1,21 @@
+//
+//  MUConstants.m
+//  MatchedUp
+//
+//  Created by Kurt Walker on 8/14/14.
+//  Copyright (c) 2014 Matched Up. All rights reserved.
+//
+
+#import "MUConstants.h"
+
+@implementation MUConstants
+
+NSString *const kCCUserProfileKey               = @"profile";
+NSString *const kCCUserProfileNameKey           = @"name";
+NSString *const kCCUserProfileFirstNameKey      = @"firstName";
+NSString *const kCCUserProfileLocationKey       = @"location";
+NSString *const kCCUserProfileGenderKey         = @"gender";
+NSString *const kCCUserProfileBirthdayKey       = @"birthday";
+NSString *const kCCUserProfileInterestedInKey   = @"interestedIn";
+
+@end
